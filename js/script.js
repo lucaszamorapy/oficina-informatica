bf9b980a74ade28332c4b3da2d61acbbbe9f4034
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2023-12-27 20:37:44 -0300

    feat: create about

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
+//menu.js
+function initMobileMenu() {
   const mobileMenuBtn = document.getElementById("mobile-menu-btn");
   const mobileMenu = document.getElementById("mobile-menu");
 
   mobileMenuBtn.addEventListener("click", function () {
     mobileMenu.classList.toggle("hidden");
   });
-});
-// Chama a função Menu sem um argumento inicialmente
+}
+initMobileMenu();
 
 // slider.js
 function Slider() {
@@ -33,3 +34,49 @@ function Slider() {
   });
 }
 Slider();
+
+function initCounters() {
+  const counters = document.querySelectorAll(".counter");
+  const speed = 200; // Velocidade da animação
+
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function activateCounters() {
+    counters.forEach((counter) => {
+      if (isElementInViewport(counter)) {
+        const target = +counter.getAttribute("data-target");
+        const inc = target / speed;
+        let count = 0;
+
+        const updateCount = () => {
+          if (count < target) {
+            counter.innerText = "+" + Math.ceil(count + inc); // Adiciona o "+"
+            count += inc;
+            setTimeout(updateCount, 1);
+          } else {
+            counter.innerText = "+" + target; // Adiciona o "+"
+          }
+        };
+
+        updateCount();
+      }
+    });
+  }
+
+  // Ativar contadores quando a página carrega
+  activateCounters();
+
+  // Ativar contadores quando o usuário rola a página
+  window.addEventListener("scroll", activateCounters);
+}
+
+initCounters();

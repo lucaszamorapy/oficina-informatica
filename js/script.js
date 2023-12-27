ccd1426dd34343bd0157a7f36cca29ab69aedb02
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2023-12-27 15:53:19 -0300

    feat: create services

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
-function Menu(e) {
-  let list = document.querySelector("ul");
-  e.name === "menu"
-    ? ((e.name = "close"),
-      list.classList.add("top-[80px]"),
-      list.classList.add("opacity-100"))
-    : ((e.name = "menu"),
-      list.classList.remove("top-[80px]"),
-      list.classList.remove("opacity-100"));
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenuBtn = document.getElementById("mobile-menu-btn");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  mobileMenuBtn.addEventListener("click", function () {
+    mobileMenu.classList.toggle("hidden");
+  });
+});
+// Chama a função Menu sem um argumento inicialmente
+
+// slider.js
+function Slider() {
+  const imagens = [
+    "/oficina-informatica/img/foto-0.png",
+    "/oficina-informatica/img/foto-1.png",
+    "/oficina-informatica/img/foto-2.png",
+    "/oficina-informatica/img/foto-3.png",
+    "/oficina-informatica/img/foto-4.png",
+  ];
+
+  const swiperWrapper = document.getElementById("swiper-wrapper");
+
+  imagens.forEach((imagem, index) => {
+    const swiperSlide = document.createElement("div");
+    swiperSlide.className = "swiper-slide border-yellowOficina100 border-2";
+
+    const img = document.createElement("img");
+    img.src = imagem;
+    img.alt = `Foto ${index}`;
+
+    swiperSlide.appendChild(img);
+    swiperWrapper.appendChild(swiperSlide);
+  });
 }
-Menu();
+Slider();

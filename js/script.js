d7ad1bc8127626f358dc73d06b766e8c9d7c0b32
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2023-12-27 21:26:26 -0300

    fix: tailwind config

--- js/script.js
+++ js/script.js
@@ -35,6 +35,7 @@ function Slider() {
 }
 Slider();
 
+//counter.js
 function initCounters() {
   const counters = document.querySelectorAll(".counter");
   const speed = 200; // Velocidade da animação
